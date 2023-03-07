ff79fd1d27ce74df9585ea8e867f9905e66a8755
thomas <tom.angotti11@gmail.com>
2023-03-07 15:11:44 -0700

    added sample imaged

--- client/src/components/RenderMessageCard.js
+++ client/src/components/RenderMessageCard.js
@@ -1,6 +1,7 @@
 function RenderMessageCard({message}) {
     return(
         <div className="message-card">
+            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" />
             <h6>{message.user.username} ... {message.created_at}</h6>
             <p>{message.content}</p>
             
